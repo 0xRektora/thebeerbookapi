f5aa6f92ddb2b0fc6fe27d1f835815d8ddb6561c
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-24 20:52:23 -0500

    test(app): impl basic app health check testing

---
+++ src/app.spec.ts
@@ -0,0 +1,18 @@
+import { Application } from 'express';
+import { createExpressServer } from 'routing-controllers';
+import request from 'supertest';
+import CONFIG from './Core/config';
+import useLoaders from './Loaders/bootstrapLoader';
+
+describe('App', () => {
+    let app: Application;
+
+    beforeAll(async () => {
+        app = createExpressServer(CONFIG.routingControllersOptions);
+        useLoaders(app);
+    });
+
+    it('Should up the server', function (done) {
+        request(app).get('/healthcheck').expect('Content-type', /json/).expect(200, done);
+    });
+});
