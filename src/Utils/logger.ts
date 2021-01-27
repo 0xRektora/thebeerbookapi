b6f87e73fa17b2be13df16146dc005695e5e644d
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-27 09:02:25 -0500

    refactor(logger): updated logger object location

--- src/Utils/logger.ts
+++
@@ -1,8 +0,0 @@
-import { transports, createLogger } from 'winston';
-import CONFIG from '../Core/config';
-
-const logger = createLogger({
-    transports: [new transports.Console(), new transports.File({ filename: CONFIG.logs.filename })],
-});
-
-export default logger;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { Application } from 'express';
 import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
+import { createLogger, transports } from 'winston';
 import CONFIG from './Core/config';
 import useLoaders from './Loaders/bootstrapLoader';
+
+export const logger = createLogger({
+    transports: [new transports.Console(), new transports.File({ filename: CONFIG.logs.filename })],
+});
 
 export const bootstrap = (app: Application): void => {
     useLoaders(app);
