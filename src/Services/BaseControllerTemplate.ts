ee03ab9e814a7e17e01cfba660a2e5e10f5c11a5
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-27 09:05:33 -0500

    feat(beer api): impl /beers & /beers/:id
    -/+ Updated BaseController to include PivoHubService insance + added helpers
    + Refactored
    BeerController, added helpers and impl /Beers API features

--- src/Services/BaseControllerTemplate.ts
+++ src/Services/BaseControllerTemplate.ts
@@ -1,16 +1,22 @@
 import { PrismaClient } from '@prisma/client';
-import axios, { AxiosInstance } from 'axios';
+import { PivoHub } from '../External/PivoHub';
 
 export abstract class BaseControllerTemplate {
     public PrismaService: PrismaClient;
-    public Axios: AxiosInstance;
+    public PivoHubService: PivoHub;
     constructor() {
         this.PrismaService = new PrismaClient();
-        this.Axios = axios.create({
-            baseURL: 'https://api.pivohub.com/public',
-            headers: {
-                Accept: 'application/json, text/plain, */*',
-            },
-        });
+        this.PivoHubService = new PivoHub();
+    }
+
+    /**
+     * Uses Prisma transactions to create many records in one DB call
+     *
+     * @param data The data to create
+     * @param mapFn The map fn for atomic creation
+     */
+    public async createMany<T, E>(data: T[], mapFn: (e: T) => E): Promise<T[]> {
+        const transactions = data.map(mapFn);
+        return ((await this.PrismaService.$transaction(transactions)) as unknown) as T[];
     }
 }
--- src/Services/Beer/BeerController.ts
+++ src/Services/Beer/BeerController.ts
@@ -1,15 +1,15 @@
 import { Beer } from '@prisma/client';
-import { IsPositive } from 'class-validator';
+import { IsDefined, IsPositive } from 'class-validator';
 import { Get, JsonController, Param, QueryParams } from 'routing-controllers';
+import { IProductsResults } from '../../External/IPivoHub';
 import { BaseControllerTemplate } from '../BaseControllerTemplate';
 
 const PATH = '/beers';
 
 class GetAllBeersQueryParams {
-    @IsPositive()
+    @IsDefined()
     limit!: number;
-
-    @IsPositive()
+    @IsDefined()
     skip!: number;
 }
 
@@ -18,33 +18,53 @@ export class BeerController extends BaseControllerTemplate {
     protected beerRoute = '/products?tags[]=the-festival-2019';
 
     @Get()
-    getAll(@QueryParams() query: GetAllBeersQueryParams): Beer[] {
-        this.Axios.get(this.beerRoute)
-            .then((e) => {
-                console.log(e.data);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-        return [
-            {
-                companyId: 'test',
-                description: 'test',
-                id: 'test',
-                imageUrl: 'test',
-                percentage: 15,
-            },
-        ];
+    async getAll(@QueryParams() query: GetAllBeersQueryParams): Promise<Beer[]> {
+        const data = await this.PivoHubService.getProducts<Beer[]>(BeerController.mapProductsAPIToBeerEntity);
+        const res = await this.hydrateDbBeerValues(data);
+
+        const resApplyParams = res.slice(query.skip).slice(0, query.limit);
+        return resApplyParams;
     }
 
     @Get('/:id')
-    get(@Param('id') id: string): Beer {
-        return {
-            companyId: 'test',
-            description: 'test',
-            id: 'test',
-            imageUrl: 'test',
-            percentage: 15,
-        };
+    async get(@Param('id') id: string): Promise<Beer | null> {
+        const beer = await this.PrismaService.beer.findUnique({
+            where: {
+                id: id,
+            },
+        });
+        return beer;
+    }
+
+    /**
+     * Create records of the passed data if needed
+     *
+     * @param data The data to hydrate the DB with
+     */
+    private async hydrateDbBeerValues(data: Beer[]): Promise<Beer[]> {
+        const beerCount = await this.PrismaService.beer.count();
+        if (!beerCount) {
+            const createManyMapFn = (e: Beer) => {
+                return this.PrismaService.beer.create({ data: e });
+            };
+            const transactions = await this.createMany(data, createManyMapFn);
+            return transactions;
+        }
+        return data;
+    }
+
+    /**
+     * Map the raw data to a DB friendly one
+     *
+     * @param data The PivoHub API data
+     */
+    private static mapProductsAPIToBeerEntity(data: IProductsResults): Beer[] {
+        return data.data.map((e) => ({
+            companyId: e.companyId,
+            id: e.id,
+            description: e.description['en-CA'],
+            imageUrl: e.imageUrl,
+            percentage: e.details.percentage,
+        }));
     }
 }
